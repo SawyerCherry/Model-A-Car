541847b422c37cfce12da28cffbb119164de27c6
Sawyer Cherry <sawyercherry@macbook-pro-78.local>
2018-08-20 14:33:20 -0500

    Initial Commit

---
+++ Model A Car/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Model A Car
+//
+//  Created by Sawyer Cherry on 8/20/18.
+//  Copyright © 2018 Sawyer Cherry. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
